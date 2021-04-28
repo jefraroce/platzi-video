0c4fc3f53c394bbcbd3f78aef8277c5f08c6673a
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-04-28 12:07:34 -0500

    Created little service to integrate gravatar.

---
+++ src/services/gravatar.js
@@ -0,0 +1,9 @@
+import md5 from 'md5';
+
+export default (email) => {
+  const base = 'https://gravatar.com/avatar/';
+  const formattedEmail = email.trim().toLowerCase();
+  const hash = md5(formattedEmail, { encoding: 'binary' });
+
+  return `${base}${hash}`;
+};
